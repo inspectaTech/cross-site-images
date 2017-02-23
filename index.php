339a20623dc33c749c0fa5a80f2d4d7c8afb98d3
inspectaTech <d3po.techengineer@gmail.com>
2017-02-23 19:12:48 +0000

    Initiate cross-site-images repo

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php  ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="css/cross_image.css" type="text/css" />
+        <script type="text/javascript" src="cross_image.js"></script>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+        <?php echo "what is this" ?>
+        
+    </body>
+</html>
